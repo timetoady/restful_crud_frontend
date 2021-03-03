8dcfce5da86362ff065587fabdd8cf1169ba7af6
ADAM ANDREASON <adamandreason@gmail.com>
2021-03-03 02:32:42 -0700

    initial svelte setup

---
+++ api.js
@@ -0,0 +1,14 @@
+//Main default fetch method
+export default async function tryCatch(URL, modifier = "", method = "GET", headers = {} ) {
+    try {
+      const response = await fetch(URL + `${modifier}`, {
+        method: method,
+        headers: headers,
+      });
+      const data = await response.json();
+	  //console.log(data)
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
